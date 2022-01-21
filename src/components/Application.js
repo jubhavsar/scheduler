b154a20fc4d602065151e697fba907b5adba2523
jubhavsar <juhi_bhavsar@yahoo.com>
2022-01-21 13:13:39 -0500

    fetch appoinments data using axios and refactor code using useEffect,promises and also call helper fuction getAppointmentsForDay()

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,62 +3,50 @@ import DayList from "components/DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 import axios from "axios";
+import getAppointmentsForDay from "helpers/selectors";
 
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
-const parsedAppointments = appointments.map((appointment) => {
-  return (
-    <Appointment key={appointment.id} {...appointment} />
-  );
-  });
 export default function Application(props) {
-  const [day, setDay] = useState("");
-  const [days, setDays] = useState([]);
+  
+  // define state
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+      
+  });
 
+  // invoking helper function that returns array of appoinments
+  let dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  //Loopover each appoinment and display each appoinment on specific day
+  const parsedAppointments = dailyAppointments.map((appointment) => {
+    return (
+     <Appointment key={appointment.id} {...appointment} />
+    );
+  });
+
+  // Implement setDay fun which set specific day for appoinments
+  const setDay = day => setState({ ...state, day });
+
+  // useEffect(() => {
+  //   axios.get(`http://localhost:8001/api/days`).then(response => {
+  //     const data = response.data;
+  //     // setDays([...data])
+  //      setDay(data[0].name)
+  //   });
+  // }, []);
+  
+// Fetching data using axios get request and useEffect with promises
   useEffect(() => {
-    axios.get(`http://localhost:8001/api/days`).then(response => {
-      // console.log("@@@@@",response.data);
-      const data = response.data;
-      setDays([...data])
-      setDay(data[0].name)
-    });
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments')
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+    }).catch((err) => {
+      console.log(err.message);
+    })
   }, []);
  
 
@@ -73,7 +61,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList  days={days}  value={day}  onChange={setDay} />
+        <DayList  days={state.days}  value={state.day}  onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
