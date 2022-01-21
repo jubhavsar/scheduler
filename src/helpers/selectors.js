76638b0c9b6b34cbef829e1d71c522ccd8e0f19b
jubhavsar <juhi_bhavsar@yahoo.com>
2022-01-20 23:11:45 -0500

    Implement selector helper function with edge cases

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+
+export function getAppointmentsForDay(state, day) {
+   
+  let dayObject = state.days.find(dayElem => dayElem.name === day);
+  
+  if(!dayObject){
+    return [];
+  }
+
+  let appointments = dayObject.appointments.map((appoinment) => {
+    let appointmentList = state.appointments[appoinment];
+    return appointmentList;
+  })
+  
+  return appointments;
+
+}
