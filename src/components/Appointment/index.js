524619a9f3d2c4687506c23715dd1ccc2b3c0f20
jubhavsar <juhi_bhavsar@yahoo.com>
2022-01-22 12:58:20 -0500

    Implement Empty,Create

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,16 +3,40 @@ import "components/Appointment/styles.scss";
 import Header from './Header';
 import Empty from './Empty';
 import Show from './Show';
+import Form from './Form'
+import useVisualMode from 'hooks/useVisualMode';
 
+
+
+//Appointment component
 const Appointment = (props) => {
   // console.log("i!!!!!", props)
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  // const array = [props.interviewers]
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
   <article className="appointment">
   <Header time={props.time}/>
-    {(props.interview)?<Show student={props.interview.student} 
-                        interviewer={props.interview.interviewer.name}/> :<Empty />}
+    {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+    {mode === SHOW && (
+      <Show
+         student={props.interview.student}
+         interviewer={props.interview.interviewer}
+       />
+      )}
+    {mode === CREATE && (
+      <Form interviewers={[]} onCancel={() => back(EMPTY)} />
+    )}
   </article>
   )
 };
 
 export default Appointment;
+
+
+ 
